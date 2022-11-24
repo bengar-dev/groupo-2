3824dce020a49115692ad9db776855b2e0787c02
bengar-dev <benoitgar.pro@gmail.com>
2022-11-24 23:25:40 +0100

    update: .env

--- back-api/src/index.ts
+++ back-api/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.send("🏠 Welcome home!"));
+app.get("/", (req, res) => res.send(`Home`));
 
-app.listen(8080, () => console.log("Silence, ça tourne. !!"));
+app.listen(process.env.API_PORT, () => {
+  console.log("🚀 Server is launching...");
+  console.log(
+    `Server is running on : ${process.env.DEV_ENV}:${process.env.API_PORT}`
+  );
+});
